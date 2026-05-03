802cdc190f6683874fb386ded5059ff54ac4a7d4
SumitRoy-Gh <sumitr82324@gmail.com>
2026-05-04 01:34:39 +0530

    setup file added

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
+'''
+This setup.py file is an essential part of packaging and distributing python projects.
+It is used by setuptools(or distutils) to define the metadata and dependencies of the project, making it easier to install and manage the project as a package.
+
+'''
+
 from setuptools import find_packages , setup 
+from typing import List
+
+def get_requirements() -> List[str]:
+    requirements_list: List[str] = []
+    try:
+        with open("requirements.txt", 'r') as file:
+            lines = file.readlines()
+
+            for line in lines:
+                requirement = line.strip()
+
+                # ignore empty lines and '-e .'
+                if requirement != '' and not requirement.startswith('-e .'):
+                    requirements_list.append(requirement)
+
+    except FileNotFoundError:
+        print("requirements.txt file not found.")
+
+    return requirements_list
+
+print(get_requirements())
 
 setup(
     name = "Network-Security-ML",
@@ -6,5 +33,5 @@ setup(
     author="Sumit",
     author_email="sumitr82324@gmail.com",
     packages = find_packages(),
-    install_requires = []
+    install_requires = get_requirements()
 )
